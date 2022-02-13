8598f78b9930fe05028c6cb0e3d47e9bdcd4d689
Nadia Shalaby <ns1133@mun.ca>
2022-02-13 15:18:32 -0330

    fix

--- rtos-alloc.c
+++ rtos-alloc.c
@@ -28,10 +28,10 @@ void initialize(){
 
 void split(struct block *fitting_slot,size_t size){
 	struct block *new=(void*)((void*)fitting_slot+size+sizeof(struct block));
- 	new->size=(fitting_slot->size)-size-sizeof(struct block);
+ 	new->block_size=(fitting_slot->size)-size-sizeof(struct block);
  	new->free=1;
  	new->next=fitting_slot->next;
- 	fitting_slot->size=size;
+ 	fitting_slot->block_size=size;
  	fitting_slot->free=0;
  	fitting_slot->next=new;
 }	
