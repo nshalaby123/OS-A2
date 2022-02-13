f1ab4f0e8028007464f49ae4ca98ebbb089c683d
Nadia Shalaby <ns1133@mun.ca>
2022-02-13 08:42:28 -0330

    update

--- rtos-alloc.c
+++ rtos-alloc.c
@@ -50,4 +50,29 @@ void	rtos_free(void *ptr){
 
 
 
+ * How large is the allocation pointed at by this pointer?
+ *
+ * @pre   @b ptr points at a valid allocation (according to `rtos_allocated`)
+ */
+size_t  rtos_alloc_size(void *ptr){
+}
 
+/**
+ * Does this pointer point at the beginning of a valid allocation?
+ *
+ * @param   ptr    any virtual address
+ *
+ * @returns whether or not @b ptr is a currently-allocated address returned
+ *          from @b my_{m,re}alloc
+ */
+bool    rtos_allocated(void *ptr){
+}
+
+/**
+ * How much memory has been allocated by this allocator?
+ *
+ * @returns the number of bytes that have been allocated to user code,
+ *          **not** including any allocator overhead
+ */
+size_t  rtos_total_allocated(void){
+}
