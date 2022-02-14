c01ad108309ee1124738e99b12242e62e1f7b0ec
Nadia Shalaby <ns1133@mun.ca>
2022-02-13 22:02:28 -0330

    ww

--- rtos-alloc.c
+++ rtos-alloc.c
@@ -143,7 +143,7 @@ void*	rtos_malloc(size_t size){
 	}
 
 	curr = freeblock;
-	while((((curr->block_size <size) || ((curr->free == 0)) && (curr->next !=NULL)))){
+	while(((((curr->block_size <size) || ((curr->free == 0))) && (curr->next !=NULL)))){
 		prev = curr;
 		curr = curr->next;
 		
